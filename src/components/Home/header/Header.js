6d1e7260f762bebada2b693794d97725f1911a6c
kailanyy <kailanyiaramenco@gmail.com>
2022-08-09 22:50:06 -0300

    Menu dropdown de perfil

--- src/components/Home/header/Header.js
+++ src/components/Home/header/Header.js
@@ -1,6 +1,13 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import {Link} from "react-router-dom";
+
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+
+
+import { Link } from "react-router-dom";
 import logo from '../../assets/logo.png'
 
 // import  SearchIcon from '@mui/icons-material/Search';
@@ -8,41 +15,64 @@ import logo from '../../assets/logo.png'
 import CloseIcon from '@mui/icons-material/Close';
 import MenuIcon from '@mui/icons-material/Menu';
 
+import person from '../../assets/person.png'
+import star from '../../assets/star.png'
+import exit from '../../assets/exit.png'
 
-const Header = ()=>{
-    const [sidebar,setSidebar] = useState(false)
 
-    window.addEventListener("scroll",function() {
-        const header = this.document.querySelector(".header")
-        header.classList.toggle("active",this.window.screenY > 200)
-    })
-    return(
-        <>
-        <header className="header">
-            <div className="container flex">
-                <div className="logo">
-                    <img src={logo} width={40} alt="logo"/>
+const Header = () => {
+  const [sidebar, setSidebar] = useState(false)
+
+  window.addEventListener("scroll", function () {
+    const header = this.document.querySelector(".header")
+    header.classList.toggle("active", this.window.screenY > 200)
+  })
+
+  return (
+    <>
+      <header className="header">
+        <div className="container flex">
+          <div className="logo">
+            <img src={logo} width={40} alt="logo" />
+          </div>
+          <div className="nav">
+            <ul className={sidebar ? "nav-links-sidebar" : "nav-links"} onClick={() => setSidebar(false)}>
+              <li>
+                <div>
+                  {[DropdownButton].map((DropdownType, idx) => (
+                    <DropdownType className='dropdownType'
+                      as={ButtonGroup}
+                      key={idx}
+                      id={`dropdown-button-drop-${idx}`}
+                      size="md"
+                      title="Meu perfil"
+                    >
+                      <Dropdown.ItemText className='img-Dropdown' eventKey="1"><img width={100} src={person} /></Dropdown.ItemText>
+                      <Dropdown.ItemText className='dropdown-ItemText'>Mario Silvo</Dropdown.ItemText>
+                      <Dropdown.Divider />
+                      <Dropdown.Item className='dropdown-ItemText' eventKey="2"><img width={20} src={star} />Ver perfil</Dropdown.Item>
+                      <Dropdown.Item className='dropdown-ItemText' eventKey="4"><img width={20} src={exit} />Sair</Dropdown.Item>
+                    </DropdownType>
+                  ))}
                 </div>
-                <div className="nav">
-                    <ul className={sidebar ? "nav-links-sidebar" : "nav-links"} onClick={() =>setSidebar(false)}>
-                        <li><Link to='/'>Inicial</Link> </li>
-                        <li><Link to='/quemsomos'>Quem somos</Link> </li>
-                        <li className="icon">
-                             {/* <SearchIcon className="HeaderIcon"/>
+              </li>
+
+              <li><Link to=''>Inicial</Link> </li>
+              <li><Link to='/quemsomos'>Quem somos</Link> </li>
+              <li className="icon">
+                {/* <SearchIcon className="HeaderIcon"/>
                              <KeyboardArrowRightIcon className="HeaderIcon"/> */}
-                             <button className="button">Entrar</button>
-                        </li>
-                    </ul>
-
-                </div>
-
-                
-                    <button className="navbar-itens-icon" onClick={() =>setSidebar(!sidebar)}>
-                        {sidebar ? <CloseIcon/> : <MenuIcon/>}
-                    </button>
-            </div>
-        </header>
-        </>
-    )
+                <button className="button">Entrar</button>
+              </li>
+            </ul>
+          </div>
+          <button className="navbar-itens-icon" onClick={() => setSidebar(!sidebar)}>
+            {sidebar ? <CloseIcon /> : <MenuIcon />}
+          </button>
+        </div>
+      </header>
+    </>
+  )
 }
 export default Header
+
