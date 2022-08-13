ae100e1d4abb8ab41031c63caa467805246ff4de
yasmin <yasmintitow@gmail.com>
2022-08-13 01:12:35 -0300

    design do container mensagens do chat feito :)))

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -4,21 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
+import SendIcon from '@mui/icons-material/SendRounded'
+
 
 const Chat = () => {
   return (
     <div className='allChat'>
       <div className='boxSearchChat'>
         <div className="containerSearch">
-          <Box className="inputSearch" sx={{ '& > :not(style)': { m: 0.1 } }}>
-            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+          <Box className="boxInputSearch">
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
               <SearchIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-              <TextField id="input-with-sx" label="Pesquisar usuário" variant="standard" />
+              <TextField id="input-with-sx" label="Pesquisar usuário" variant="standard" className='inputSearchChat'/>
             </Box>
           </Box>
-          <button className="buttonSearchChat">BUSCAR</button>
+          
+          <button className="buttonSearchChat">Buscar</button>
         </div>
       </div>
+
       <div className='containerChat'>
         <div className='headerChat'>
           <img src='https://cdn-icons-png.flaticon.com/512/3135/3135715.png' id='iconIndividualChat' alt="Profile" />
@@ -28,12 +32,22 @@ const Chat = () => {
         <div className='messagesChat'>
           <p id='leftMessage'>Lorem ipsum dolor sit amet?</p>
           <p id='rightMessage'>Aliquam lacinia, magna quis</p>
+
+          <p id='leftMessage'>Justo varius magna</p>
+          <p id='rightMessage'>Vitae dapibus!</p>
+
+          <p id='leftMessage'>Dolorem ipsum quia dolor :)</p>
+          <p id='rightMessage'>Fermentum felis pellentesque congue</p>
         </div>
 
-        <input
-          className='typeMessage'
-          placeholder='Digite sua mensagem aqui'
-        />
+        <div className='typeMessage'>
+          <input
+            className='inputMessage'
+            placeholder='Digite sua mensagem aqui'
+          />
+          
+          <SendIcon id='sendIcon'/>
+        </div>
       </div>
     </div>
   )
