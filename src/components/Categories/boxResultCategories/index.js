b0656d34547f5db8b0b1ad31d56003b2a4ab8adb
kailanyy <kailanyiaramenco@gmail.com>
2022-08-19 13:40:14 -0300

    Alteração na box de resultado de categorias

--- src/components/Categories/boxResultCategories/index.js
+++ src/components/Categories/boxResultCategories/index.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import "./styles.css"
 import PlaceIcon from '@mui/icons-material/Place';
+import Grid from '@mui/material/Grid';
+import Categories from '..';
 
 const ResultCategories = () => {
   return (
     <>
-      <div className="resultCategories">
-        <div className="containerSearch">
-          <h1>Jardineiro</h1>
-          <div className='input-search'>
-            <PlaceIcon />
-            <input
-              name="fullName"
-              placeholder='Nome completo'
-            />
-          </div>
-          <button className="buttonSearch">Buscar</button>
-        </div>
+      <div className='container-search'>
+        <Grid container spacing={2} columns={16} className='grid-search'>
+          <Grid item xs={8} >
+            <div className='input-search'>
+              <PlaceIcon id='icon-search' />
+              <input
+                className='input-search-content'
+                type={'text'}
+                placeholder='Onde?'
+              />
+            </div>
+          </Grid>
+          <Grid item xs={8}>
+            <button className='button-search'>Buscar</button>
+          </Grid>
+        </Grid>
       </div>
     </>
   )
