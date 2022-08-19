5a087c4e25830ae61c7119d34fe9422935d59e51
yasmin <yasmintitow@gmail.com>
2022-08-19 01:02:21 -0300

    de quebra fiz a de excluir os serviços cadastrados tb; amanhã add o resto das validações :wastebasket:

--- src/components/Profile/Modal/CancelService.js
+++ src/components/Profile/Modal/CancelService.js
@@ -1,18 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles.css'
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
-
 import CancelServiceIcon from '@mui/icons-material/HighlightOffRounded'
+import api from "../../../api";
 
 const CancelService = (props) => {
     const [show, setShow] = useState(false);
+    const [selectValue, setSelectValue] = useState(''); 
+    const [services, setServices] = useState([]);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    async function deleteService() {
+      try {
+          if (selectValue !== 'Serviços') {
+          const idPerson = 1;
+          const response = await api.delete(`/workers/${idPerson}`, {data: {idService: selectValue}})
+          console.log('response', response)
+          
+          } else {
+          console.log('campo nao preenchiddddo');
+          }
+      } catch (error) {
+
+      }
+    }
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        const response = await api.get('/services');
+        setServices(response.data)
+      }
+      fetchData()
+        .catch(console.error);
+    }, [])
 
     return(
         <div>
@@ -33,16 +58,24 @@ const CancelService = (props) => {
             <Modal.Body>
                 <p>Ao cancelar um serviço o seu perfil na categoria específica vai ser excluído.</p>
 
-                <Form.Select autoFocus>
-                  <option>Selecione um serviço</option>
-                  <option value="1">Eletricista</option>
-                  <option value="2">Jardineiro</option>
-                  <option value="3">Babá</option>
-                </Form.Select>          
+                <Form.Select
+                autoFocus
+                value={selectValue}
+                onChange={e => setSelectValue(e.target.value)}
+                >
+                    <option>Serviços</option>
+                    {
+                        services.map((item) => {
+                            return (
+                                <option key={item.idservice} value={item.idservice}>{item.titleservice}</option>
+                            )
+                        })
+                    }
+                </Form.Select>         
             </Modal.Body>
 
             <Modal.Footer>
-              <Button variant="danger" onClick={handleClose}>
+              <Button variant="danger" onClick={() => deleteService()}>
                 Cancelar serviço
               </Button>
             </Modal.Footer>
