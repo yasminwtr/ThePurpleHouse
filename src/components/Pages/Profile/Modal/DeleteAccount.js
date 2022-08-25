68b898528a4282ac3faf2bea6c9292699a8e8d6b
stefani <stefani_l_ferreira@estudante.sesisenai.org.br>
2022-08-25 17:02:57 -0300

    deletar conta

--- src/components/Pages/Profile/Modal/DeleteAccount.js
+++ src/components/Pages/Profile/Modal/DeleteAccount.js
@@ -5,29 +5,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import AuthContext from '../../../contexts/auth'
+
+
 
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
-
-import AuthContext from "../../../contexts/auth";
-
 import DeleteAccountIcon from '@mui/icons-material/DeleteForeverRounded'
 
 const DeleteAccount = (props) => {
 
  
-
+    
     const [show, setShow] = useState(false);
     const [showError, setShowError] = useState(false);
+   
     const [showSuccess, setShowSuccess] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    
+    
     const handleCloseError = () => setShowError(false);
     const handleCloseSuccess = () => setShowSuccess(false);
 
-    const { user } = useContext(AuthContext);
-    const [person, setPerson] = useState([])
+    const { user, signOut  } = useContext(AuthContext);
+    const [person, setPerson] = useState([]);
+    const [password, setPassword] = useState('')
 
     const Alert = React.forwardRef(function Alert(props, ref) {
       return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
@@ -42,11 +46,23 @@ const DeleteAccount = (props) => {
         console.log(deleteId)
         await fetch('http://localhost:3000/users/' + deleteId, requestOptions)
         setPerson(person.filter(person => person.idPerson !== deleteId))
-        setShowSuccess(true)
-        setShow(false)
+        console.log('funfou');
       
       } catch (error) {
+        console.log(error)
+      }
+    }
+
+    function validationPass() {
+      if (password === user.pass) {
+        deleteUser(user.idperson)
+        signOut()
+        // setShowSuccess(true)
+        // setShow(false)
+        //console.log(user, 'aaaaa');
+      } else {
         setShowError(true)
+        //console.log('errou');
       }
     }
 
@@ -77,13 +93,18 @@ const DeleteAccount = (props) => {
                             placeholder="******"
                             autoFocus
                             maxLength={25}
+                            id = 'password'
+                            onChange={(event) => setPassword(event.target.value)}
                         />
                     </Form.Group>
                 </Form>
             </Modal.Body>
 
             <Modal.Footer>
-              <Button variant="danger" onClick={() => {deleteUser(user.idperson)}}>
+              <Button variant="danger" 
+              //onClick={() => {deleteUser(user.idperson)}}
+              onClick={() => {validationPass()}}
+              >
                 Excluir
               </Button>
             </Modal.Footer>
@@ -91,10 +112,11 @@ const DeleteAccount = (props) => {
 
           <Snackbar open={showError} autoHideDuration={6000} onClose={handleCloseError} anchorOrigin={{ vertical: 'bottom', horizontal: 'center', }}>
             <Alert onClose={handleCloseError} severity="error" sx={{ width: '100%', fontFamily: 'Inter-Regular' }}>
-              Preencha o formulário para editar sua conta
+            Erro ao excluir a conta 
             </Alert>
           </Snackbar>
 
+        
           <Snackbar open={showSuccess} autoHideDuration={6000} onClose={handleCloseSuccess} anchorOrigin={{ vertical: 'bottom', horizontal: 'center', }}>
             <Alert onClose={handleCloseSuccess} severity="success" sx={{ width: '100%', fontFamily: 'Inter-Regular' }}>
             Conta excluída com sucesso!
