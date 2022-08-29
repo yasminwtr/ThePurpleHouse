fb33ec0dcf634edfee164d398ce4f2d560e87a9f
kailanyy <kailanyiaramenco@gmail.com>
2022-08-29 17:50:34 -0300

    Ínicio pro header não aparecer em login/cadastro :')

--- src/components/services/routes/auth.routes.js
+++ src/components/services/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, BrowserRouter, Switch, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Header from "../../Header/"
 import Footer from "../../Footer";
 import Home from "../../Pages/Home/"
@@ -7,17 +7,26 @@ import About from "../../Pages/About";
 import Registration from "../../Pages/Registration"
 import Login from "../../Pages/Login"
 
+// const Layout = ({children}) => (
+//   <div>
+//     <Header />
+//     {children}
+//   </div>
+// )
+
 const AuthRoutes = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Registration" element={<Registration />} />
+    <BrowserRouter forceRefresh={true}>
+      {/* <Header /> */}
+      {window.location.pathname !== "/Login" ? <Header /> : null}
+      <Routes forceRefresh={true}>
+        {/* <Layout/> */}
+        <Route forceRefresh={true} index element={<Home />} />
+        <Route forceRefresh={true} path="/About" element={<About />} />
+        <Route forceRefresh={true} path="/Login" element={<Login />} />
+        <Route forceRefresh={true} path="/Registration" element={<Registration />} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </BrowserRouter >
   )
 }
