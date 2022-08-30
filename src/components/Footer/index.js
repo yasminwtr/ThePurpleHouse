f4aa18bd2276cfd42c1871554bf1d821e2ba349a
kailanyy <kailanyiaramenco@gmail.com>
2022-08-29 22:34:25 -0300

    Header não aparece em Login/Cadastro :truck:

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,37 +1,43 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo1 from '../assets/img/logo1.png'
 import AuthContext from '../contexts/auth'
 import './styles.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Footer = () => {
-  const { signed } = useContext(AuthContext);
+  const { signed } = useContext(AuthContext)
+
+  const location = useLocation()
 
   return (
     <>
-      <footer className="footer">
-        <div className="container-footer">
-          <div className="footer-logo">
-            <img src={logo1} width={30} alt="logo1" />
+      {location.pathname !== "/Login" &&  location.pathname !== "/Registration"
+        ?
+        <footer className="footer">
+          <div className="container-footer">
+            <div className="footer-logo">
+              <img src={logo1} width={30} alt="logo1" />
+            </div>
+            <div className="div-links">
+              <ul className='footer-links'>
+                {signed ?
+                  <>
+                    <li><Link to='/About'>Sobre</Link> </li>
+                  </>
+                  :
+                  <>
+                    <li><Link to='/'>Inicial</Link> </li>
+                    <li><Link to='/About'>Sobre</Link> </li>
+                    <li><Link to='/Registration'>Fazer parte</Link> </li>
+                  </>
+                }
+              </ul>
+            </div>
           </div>
-          <div className="div-links">
-            <ul className='footer-links'>
-              {signed ?
-                <>
-                  <li><Link to='/About'>Sobre</Link> </li>
-                </>
-                :
-                <>
-                  <li><Link to='/'>Inicial</Link> </li>
-                  <li><Link to='/About'>Sobre</Link> </li>
-                  <li><Link to='/Registration'>Fazer parte</Link> </li>
-                </>
-              }
-            </ul>
-          </div>
-        </div>
-      </footer>
+        </footer>
+        : null
+      }
     </>
   )
 }
--- src/components/services/routes/auth.routes.js
+++ src/components/services/routes/auth.routes.js
@@ -7,20 +7,11 @@ import About from "../../Pages/About";
 import Registration from "../../Pages/Registration"
 import Login from "../../Pages/Login"
 
-// const Layout = ({children}) => (
-//   <div>
-//     <Header />
-//     {children}
-//   </div>
-// )
-
 const AuthRoutes = () => {
   return (
     <BrowserRouter forceRefresh={true}>
-      {/* <Header /> */}
-      {window.location.pathname !== "/Login" ? <Header /> : null}
+      <Header/>
       <Routes forceRefresh={true}>
-        {/* <Layout/> */}
         <Route forceRefresh={true} index element={<Home />} />
         <Route forceRefresh={true} path="/About" element={<About />} />
         <Route forceRefresh={true} path="/Login" element={<Login />} />
