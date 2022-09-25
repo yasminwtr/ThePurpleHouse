4d3d97a047c1a054cf9e5ee25e9d617729d613bb
yasmin <yasmintitow@gmail.com>
2022-09-25 17:11:21 -0300

    usuario agr continua logado msm depois atualizar a pagkkkkkkkk

--- src/services/contexts/auth.js
+++ src/services/contexts/auth.js
@@ -1,26 +1,40 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import * as auth from "../auth";
 
 const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
-  
+
   const [user, setUser] = useState(null);
   console.log("user @ contexts/auth.js, ", user);
 
   async function signIn(loginData) {
     try {
       const loggedUser = await auth.signIn(loginData);
-      console.log('loginData',loginData); 
-  
+      console.log('loginData', loginData);
+
       setUser(loggedUser);
+      localStorage.setItem('storagedUser', JSON.stringify(loggedUser));
     } catch (error) {
       return true
     }
   }
 
+  useEffect(() => {
+     function loadStorageData() {
+      const storagedUser = localStorage.getItem('storagedUser');
+
+      if (storagedUser) {
+        setUser(JSON.parse(storagedUser));
+      }
+    }
+
+    loadStorageData();
+  }, []);
+
   async function signOut() {
     setUser(null);
+    localStorage.clear()
   }
 
   return (
