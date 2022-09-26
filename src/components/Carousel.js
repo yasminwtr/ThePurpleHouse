fc3f911adb46da9f6bbaf7804b093bcac6d806ed
kailanyy <kailanyiaramenco@gmail.com>
2022-09-26 13:42:56 -0300

    Carousel finalizado

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -12,58 +12,31 @@ export const Carousel = () => {
 
   const [image, setImage] = useState([])
 
-  const images = [
-    {
-      original: 'https://picsum.photos/id/1018/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1018/250/150/',
-    },
-    {
-      original: 'https://picsum.photos/id/1015/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1015/250/150/',
-    },
-    {
-      original: 'https://picsum.photos/id/1019/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1019/250/150/',
-    },
-    {
-      original: 'https://picsum.photos/id/1019/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1019/250/150/',
-    },
-    {
-      original: 'https://picsum.photos/id/1019/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1019/250/150/',
-    }
-  ];
-
   const showImage = async () => {
     try {
       const response = await api.get(`/getImageWorker/${idWorker}`, { idWorker: idWorker });
 
-      setImage(response.data)
+      setImage(response.data.map((image) => {
+        return { original: image.img, thumbnail: image.img }
+      }))
 
     } catch (error) {
       console.log('error', error);
     }
   }
 
-  const log = () => console.log('image', image);
-
   useEffect(() => {
     showImage()
-  }, [])
+  }, [image])
 
   return (
     <div className='gallery'>
 
-      {/* {image.map((image, index) => (<img key={index} src={image.img} width={104} />))} */}
-
       <p id='title-blocks'>Galeria de serviços</p>
-      {/* <button onClick={log}>OOOOOOIIIII</button> */}
 
       <UploadImage />
 
-      <ImageGallery items={images} className='carousel' sizes={10} />
-      {/* {image.map((image, index) => (<img key={index} src={image.img} width={104} />))} */}
+      <ImageGallery items={image} className='carousel' sizes={10} />
 
     </div>
   )
