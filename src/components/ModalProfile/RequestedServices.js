c16038caa7ee5cdcb186c2b3a4f4ae4f57318118
KAILANY IARAMENCO SOUZA <kailany_i_souza@estudante.sesisenai.org.br>
2022-10-27 15:35:59 -0300

    Serviços solicitados de usuário e trabalhador em andamento

--- src/components/ModalProfile/RequestedServices.js
+++ src/components/ModalProfile/RequestedServices.js
@@ -15,6 +15,9 @@ const RequestedServices = (props) => {
   const [show, setShow] = useState(false);
   const navigate = useNavigate();
 
+  const [requestedServicesWorker, setRequestedServicesWorker] = useState(false);
+  const [requestedServicesUser, setRequestedServicesUser] = useState(false);
+
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
@@ -88,44 +91,59 @@ const RequestedServices = (props) => {
           </>
         }
 
-        <Modal.Body className='modal-requested-services'
-        >
-          <p>{requestedServicesCount}</p>
-          {
-            requestedServices.map((worker) => {
-              if (worker.idperson1 == user.idperson) {
-                return <>
-                  <div key={worker.idChat}>
-                    <div className='block-requested-services'>
-                      <div className='part1-avaliation-modal'>
-                        <img src='https://cdn-icons-png.flaticon.com/512/3135/3135715.png' id='icon-avaliation-modal' alt="Profile" />
+        <div className='div-buttons-modal'>
+          <Button className={requestedServicesWorker ? 'requested-service-buttons-modal-selected mb-3 mt-3 ' : 'requested-service-buttons-modal mb-3 mt-3'} onClick={() => setRequestedServicesWorker(true)}>
+            Usuários que solicitaram o meu serviço
+          </Button>
+          <Button className={requestedServicesUser ? 'requested-service-buttons-modal-selected' : 'requested-service-buttons-modal mb-3'} onClick={() => setRequestedServicesUser(true)}>
+            Serviços que eu solicitei
+          </Button>
+        </div>
+
+        {requestedServicesWorker ?
+
+          <Modal.Body className='modal-requested-services'
+          >
+            <p>{requestedServicesCount}</p>
+            {
+              requestedServices.map((worker) => {
+                if (worker.idperson1 == user.idperson) {
+                  return <>
+                    <div key={worker.idChat}>
+                      <div className='block-requested-services'>
+                        <div className='part1-avaliation-modal'>
+                          <img src='https://cdn-icons-png.flaticon.com/512/3135/3135715.png' id='icon-avaliation-modal' alt="Profile" />
+                        </div>
+                        <label onClick={() => navigate('/WorkerProfile', { state: { workerId: worker.idworker, personWorkerId: worker.idperson, firstName: worker.firstnameperson2, lastName: worker.lastnameperson2, service: worker.servicecategory, email: worker.email, phone: worker.phonenumber, birthdate: worker.birthdate, city: worker.city, cityState: worker.localization, price: worker.priceservice, description: worker.descriptionservice, whatsapp: worker.whatsapp } })}
+                          className='label-requested-info' >
+                          {worker.firstnameperson2} {worker.lastnameperson2}, {worker.servicecategory}
+                        </label>
+                        <label className='label-status'> {worker.status} </label>
                       </div>
-                      <label onClick={() => navigate('/WorkerProfile', { state: { workerId: worker.idworker, personWorkerId: worker.idperson, firstName: worker.firstnameperson2, lastName: worker.lastnameperson2, service: worker.servicecategory, email: worker.email, phone: worker.phonenumber, birthdate: worker.birthdate, city: worker.city, cityState: worker.localization, price: worker.priceservice, description: worker.descriptionservice, whatsapp: worker.whatsapp } })}
-                        className='label-requested-info' >
-                        {worker.firstnameperson2} {worker.lastnameperson2}, {worker.servicecategory}
-                      </label>
-                      <label className='label-status'> {worker.status} </label>
                     </div>
-                  </div>
-                </>
-              } else {
-                return <>
-                  <div key={worker.idChat}>
-                    <div className='block-requested-services'>
-                      <div className='part1-avaliation-modal'>
-                        <img src='https://cdn-icons-png.flaticon.com/512/3135/3135715.png' id='icon-avaliation-modal' alt="Profile" />
+                  </>
+                } else {
+                  return <>
+                    <div key={worker.idChat}>
+                      <div className='block-requested-services'>
+                        <div className='part1-avaliation-modal'>
+                          <img src='https://cdn-icons-png.flaticon.com/512/3135/3135715.png' id='icon-avaliation-modal' alt="Profile" />
+                        </div>
+                        <label className='label-requested-info' >
+                          {worker.firstnameperson1} {worker.lastnameperson1}, {worker.servicecategory}
+                        </label>
+                        <label className='label-status' > {worker.status} </label>
                       </div>
-                      <label className='label-requested-info' >
-                        {worker.firstnameperson1} {worker.lastnameperson1}, {worker.servicecategory}
-                      </label>
-                      <label className='label-status' > {worker.status} </label>
                     </div>
-                  </div>
-                </>
-              }
-            })
-          }
-        </Modal.Body>
+                  </>
+                }
+              })
+            }
+          </Modal.Body>
+          :
+          <>
+          </>
+        }
 
         <Modal.Footer>
           <Button id='close-button-modals-profile' onClick={handleClose}>
