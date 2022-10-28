90ae89a25b988e82a279c56f0fd5dd6ab536bed0
kailanyy <kailanyiaramenco@gmail.com>
2022-10-28 11:31:59 -0300

    Refatoração de workerReview

--- src/components/Reviews/WorkerReviews.js
+++ src/components/Reviews/WorkerReviews.js
@@ -1,140 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import api from '../../api';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FaStar } from 'react-icons/fa'
-import { useLocation } from 'react-router-dom';
+import AverageRating from './AverageRating';
 
 const WorkerReviews = (props) => {
   const { workerReviews } = props
 
   return (
-    <div>
-      {
-        workerReviews.map((review) => {
-          if (review.stars == 5) {
-            const [year, month, day] = review.datereview.split("T", 10)[0]?.split("-")
-            const formattedDateReview = `${day}/${month}/${year}`
+    <>
+      {workerReviews.map((review) => {
+        const [year, month, day] = review.datereview.split("T", 10)[0]?.split("-")
+        const formattedDateReview = `${day}/${month}/${year}`
 
-            return <>
-              <div key={review.idreview}>
-                <div className='block-avaliation'>
-                  <div className='part1-avaliation'>
-                    <img src='https://cdn-icons-png.flaticon.com/512/3135/3135715.png' id='icon-avaliation' alt="Profile" />
-                  </div>
-
-                  <div className='all-info-avaliation'>
-                    <div className='info-avaliation'>
-                      <p id='name-avaliation'>{review.firstnameperson} {review.lastnameperson}</p>
-                      <p id='date-avaliation'>{formattedDateReview}</p>
-                    </div>
-
-                    <div className='stars-worker-profile-review'><FaStar color='#fccc3e' size={18} /><FaStar color='#fccc3e' size={18} /><FaStar color='#fccc3e' size={18} /><FaStar color='#fccc3e' size={18} /><FaStar color='#fccc3e' size={18} /></div>
-                  </div>
+        return (
+          <div key={review.idreview}>
+            <div className='block-avaliation'>
+              <div className='part1-avaliation'>
+                <img src='https://cdn-icons-png.flaticon.com/512/3135/3135715.png'
+                  id='icon-avaliation' alt="Profile" />
+              </div>
+              <div className='all-info-avaliation'>
+                <div className='info-avaliation'>
+                  <p id='name-avaliation'>
+                    {review.firstnameperson} {review.lastnameperson}
+                  </p>
+                  <p id='date-avaliation'>
+                    {formattedDateReview}
+                  </p>
                 </div>
-
-                <p id='text-avaliation'>{review.messagereview}</p>
-              </div></>
-
-          } else if (review.stars == 4) {
-            const [year, month, day] = review.datereview.split("T", 10)[0]?.split("-")
-            const formattedDateReview = `${day}/${month}/${year}`
-
-            return <>
-              <div key={review.idreview}>
-                <div className='block-avaliation'>
-                  <div className='part1-avaliation'>
-                    <img src='https://cdn-icons-png.flaticon.com/512/3135/3135715.png' id='icon-avaliation' alt="Profile" />
-                  </div>
-
-                  <div className='all-info-avaliation'>
-                    <div className='info-avaliation'>
-                      <p id='name-avaliation'>{review.firstnameperson} {review.lastnameperson}</p>
-                      <p id='date-avaliation'>{formattedDateReview}</p>
-                    </div>
-
-                    <div className='stars-worker-profile-review'><FaStar color='#fccc3e' size={18} /><FaStar color='#fccc3e' size={18} /><FaStar color='#fccc3e' size={18} /><FaStar color='#fccc3e' size={18} /><FaStar color='#d9d9d9' size={18} /></div>
-                  </div>
-                </div>
-
-                <p id='text-avaliation'>{review.messagereview}</p>
-              </div></>
-
-          } else if (review.stars == 3) {
-            const [year, month, day] = review.datereview.split("T", 10)[0]?.split("-")
-            const formattedDateReview = `${day}/${month}/${year}`
-
-            return <>
-              <div key={review.idreview}>
-                <div className='block-avaliation'>
-                  <div className='part1-avaliation'>
-                    <img src='https://cdn-icons-png.flaticon.com/512/3135/3135715.png' id='icon-avaliation' alt="Profile" />
-                  </div>
-
-                  <div className='all-info-avaliation'>
-                    <div className='info-avaliation'>
-                      <p id='name-avaliation'>{review.firstnameperson} {review.lastnameperson}</p>
-                      <p id='date-avaliation'>{formattedDateReview}</p>
-                    </div>
-
-                    <div className='stars-worker-profile-review'><FaStar color='#fccc3e' size={18} /><FaStar color='#fccc3e' size={18} /><FaStar color='#fccc3e' size={18} /><FaStar color='#d9d9d9' size={18} /><FaStar color='#d9d9d9' size={18} /></div>
-                  </div>
-                </div>
-
-                <p id='text-avaliation'>{review.messagereview}</p>
-              </div></>
-
-          } else if (review.stars == 2) {
-            const [year, month, day] = review.datereview.split("T", 10)[0]?.split("-")
-            const formattedDateReview = `${day}/${month}/${year}`
-
-            return <>
-              <div key={review.idreview}>
-                <div className='block-avaliation'>
-                  <div className='part1-avaliation'>
-                    <img src='https://cdn-icons-png.flaticon.com/512/3135/3135715.png' id='icon-avaliation' alt="Profile" />
-                  </div>
-
-                  <div className='all-info-avaliation'>
-                    <div className='info-avaliation'>
-                      <p id='name-avaliation'>{review.firstnameperson} {review.lastnameperson}</p>
-                      <p id='date-avaliation'>{formattedDateReview}</p>
-                    </div>
-
-                    <div className='stars-worker-profile-review'><FaStar color='#fccc3e' size={18} /><FaStar color='#fccc3e' size={18} /><FaStar color='#d9d9d9' size={18} /><FaStar color='#d9d9d9' size={18} /><FaStar color='#d9d9d9' size={18} /></div>
-                  </div>
-                </div>
-
-                <p id='text-avaliation'>{review.messagereview}</p>
-              </div></>
-
-          } else if (review.stars == 1) {
-            const [year, month, day] = review.datereview.split("T", 10)[0]?.split("-")
-            const formattedDateReview = `${day}/${month}/${year}`
-
-            return <>
-              <div key={review.idreview}>
-                <div className='block-avaliation'>
-                  <div className='part1-avaliation'>
-                    <img src='https://cdn-icons-png.flaticon.com/512/3135/3135715.png' id='icon-avaliation' alt="Profile" />
-                  </div>
-
-                  <div className='all-info-avaliation'>
-                    <div className='info-avaliation'>
-                      <p id='name-avaliation'>{review.firstnameperson} {review.lastnameperson}</p>
-                      <p id='date-avaliation'>{formattedDateReview}</p>
-                    </div>
-
-                    <div className='stars-worker-profile-review'><FaStar color='#fccc3e' size={18} /><FaStar color='#d9d9d9' size={18} /><FaStar color='#d9d9d9' size={18} /><FaStar color='#d9d9d9' size={18} /><FaStar color='#d9d9d9' size={18} /></div>
-                  </div>
-                </div>
-
-                <p id='text-avaliation'>{review.messagereview}</p>
-              </div></>
-
-          }
-        })
+                <AverageRating />
+              </div>
+            </div>
+            <p id='text-avaliation'>
+              {review.messagereview}
+            </p>
+          </div>
+        )
+      })
       }
-    </div>
+    </>
   )
 }
 
